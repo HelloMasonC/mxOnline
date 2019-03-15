af4b8db508adde2f5a039ac4b3e5bf6145981105
maxuesen <13802882934@139.com>
2019-03-15 16:59:14 +0800

    update

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -4,10 +4,13 @@ from django.conf.urls import url, include
 
 from .views import OrgView, AddUserAskView
 
+
 __author__ = 'mc'
+
 
 urlpatterns = [
     # 课程机构列表页
     url(r'^list/$', OrgView.as_view(), name='org_list'),
     url(r'^add_ask/$', AddUserAskView.as_view(), name='add_ask'),
+    url(r'^home/(?P<org_id>\d+)/$', ),
 ]
